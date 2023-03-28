b1d4457265dac3daebc69b481a6d0e6f657f0e2f
rzanardi <44758033+rzanardi@users.noreply.github.com>
2023-03-28 09:47:37 +0200

    Delete restricted-access.php

--- admin/restricted-access.php
+++
@@ -1,8 +0,0 @@
-<div class="wrap">
-	<h1 id="add-new-user">
-		<?php _e( 'Access denied', 'store-location' );?>
-	</h1>
-	<p>
-		<?php _e( 'Sorry you do not have permission to access this page', 'store-location' );?>
-	</p>
-</div> 
