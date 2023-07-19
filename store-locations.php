859d8883ab3e24d313f8db6f740d83860f794c77
Premiumbizthemes <Premiumbizthemes@users.noreply.github.com>
2023-07-19 13:35:37 -0400

    Update store-locations.php
    - Change plugin version
    - Add plugin version variable to help with detecting if plugin needs to be updated

--- store-locations.php
+++ store-locations.php
@@ -6,11 +6,11 @@
 
  *	Description: An easy to use store locations management system that give users to search for stores. [store-locations]
 
- *	Version: 2.2
+ *	Version: 2.3
  */
 
 
-
+define( 'LOCATION_PLUGIN_VERSION', '2.3' );
 define( 'LOCATION_DIR_URI', plugin_dir_url( __FILE__ ));
 define( 'LOCATION_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'DISTANCE_MILES', 50);
