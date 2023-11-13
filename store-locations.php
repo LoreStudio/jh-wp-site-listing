716a0f7dccc4d4f113f9e1053e3880e941216e86
roshaneforde <roshaneforde@gmail.com>
2023-11-13 13:03:00 -0500

    Update version number to 3.2

--- store-locations.php
+++ store-locations.php
@@ -6,7 +6,7 @@
 
  *	Description: A custom locations management system, that allows patients to search and view participating study sites near them. [store-locations map=yes]
 
- *	Version: 3.1
+ *	Version: 3.2
  */
 
 
