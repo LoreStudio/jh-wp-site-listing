7bd223c707a18d0cc33bdd439380fd9035508ee6
roshaneforde <roshaneforde@gmail.com>
2024-05-11 11:16:43 -0500

    Increment version number

--- store-locations.php
+++ store-locations.php
@@ -6,11 +6,11 @@
 
  *	Description: A custom locations management system, that allows patients to search and view participating study sites near them. [store-locations map=yes]
 
- *	Version: 3.6
+ *	Version: 3.7
  */
 
 
-define( 'LOCATION_PLUGIN_VERSION', '3.5' );
+define( 'LOCATION_PLUGIN_VERSION', '3.7' );
 define( 'LOCATION_DIR_URI', plugin_dir_url( __FILE__ ));
 define( 'LOCATION_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'DISTANCE_MILES', 50);
